97dfa53629d992a17b09b9ff5585f92893dcce88
Juan Pablo Ocampo Rombolá <juanp.ocampo@hotmail.com>
2022-01-04 18:32:04 -0300

    clase05 Finalizado

--- src/app/services/productServices.js
+++ src/app/services/productServices.js
@@ -4,12 +4,19 @@ export const getProductById = (id) => {
   const productFromDB = json.products.find((element) => element.id == id);
   return productFromDB;
 };
-
+export const addProduct = ({ product }) => {
+  const { title, price, category } = product;
+  db.cart.add({
+    title,
+    price,
+    category,
+  });
+};
 export const getAllCartProducts = async () => {
   const productDB = await db.cart.toArray();
   return productDB;
 };
 
 export const deleteProduct = async (pk) => {
-  await db.cart.where('id').equals(pk).delete();
+  await db.cart.where("id").equals(pk).delete();
 };
--- src/views/ProductView.js
+++ src/views/ProductView.js
@@ -1,7 +1,10 @@
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { Container } from "react-bootstrap";
-import { getProductById } from "../app/services/productServices";
+import { Badge, Col, Container, Row, Button } from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
+import { addProduct, getProductById } from "../app/services/productServices";
 
 export const ProductView = ({ match }) => {
   const [product, setProduct] = useState(null);
@@ -10,11 +13,39 @@ export const ProductView = ({ match }) => {
     const { params } = match;
     setProduct(getProductById(params.id));
   }, []);
-
+  const addPoductToCart = ({ product }) => {
+    addProduct({ product });
+  };
   return (
     <Container>
-      <h1>Información del producto</h1>
-      <p>{JSON.stringify(product) ?? "No hay Producto"}</p>
+      <h1 className="text-primary">{product?.title}</h1>
+      <Row>
+        <Col xs={4}>
+          <img src={product?.image} alt="ImagenDelProducto"></img>
+        </Col>
+        <Col xs={4}>
+          <h4 className="text-info">{product?.category}</h4>
+          <p>{product?.description}</p>
+          <div className="row">
+            <Button
+              className="col-4 m-auto"
+              variant="warning"
+              onClick={() => addPoductToCart({ product })}
+            >
+              <FontAwesomeIcon icon={faCartPlus} />
+            </Button>
+            <Button className="col-4 m-auto" variant="primary">
+              <Badge bg="secondary">$ {product?.price}</Badge>
+              <span className="visually-hidden"></span>
+            </Button>
+          </div>
+        </Col>
+        <Rating ratingValue={product?.rating.rate * 20} readonly />{" "}
+        <span>{product?.rating.rate}</span>
+        <label className="text-secondary">
+          Vistas: {product?.rating.count}
+        </label>
+      </Row>
     </Container>
   );
 };
