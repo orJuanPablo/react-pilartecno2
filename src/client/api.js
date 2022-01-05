6307a847bd53a9b6b562efad8d89e78c7d372516
juanPocampo <juanp.ocampo@hotmail.com>
2022-01-05 10:33:28 -0300

    refactor api.js

--- src/client/api.js
+++ src/client/api.js
@@ -1,21 +1,27 @@
-const get = async (url) => {
+const execute = async (url, config = {}) => {
   try {
-    const res = await fetch(url);
+    const res = await fetch(url, config);
     return res.json();
   } catch (error) {
     throw new Error(error);
   }
 };
+const get = async (url) => {
+  return execute(url);
+};
 const post = async (url, body) => {
-  try {
-    const res = await fetch(url, { method: "POST", body });
-    return res.json();
-  } catch (error) {
-    throw new Error(error);
-  }
+  return execute(url, { method: "POST", body });
+};
+const put = async (url, body) => {
+  return execute(url, { method: "PUT", body });
+};
+const delet = async (url) => {
+  return execute(url, { method: "DELETE" });
 };
 const api = {
   get: (url) => get(url),
   post: (url, body) => post(url, body),
+  put: (url, body) => put(url, body),
+  delete: (url) => delet(url),
 };
 export default api;
